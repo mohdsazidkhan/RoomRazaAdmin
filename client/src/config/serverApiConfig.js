ea372a6567636f84e698f6d51febe7b4c3277b6a
SazidKhan <khanmohdsazid@gmail.com>
2024-06-17 13:06:30 +0530

    New Build

--- client/src/config/serverApiConfig.js
+++ client/src/config/serverApiConfig.js
@@ -6,7 +6,7 @@ console.log(
 export const API_BASE_URL =
 process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://erp-crm.idurarapp.com/api/"
+    ? "https://admin-roomraza.vercel.app/api/"
     : "http://localhost:4000/api/"
 
 export const ACCESS_TOKEN_NAME = "token"
