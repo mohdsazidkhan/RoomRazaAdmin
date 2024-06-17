804ad67fa4ed546a28d2e184fe2724ccb2f68146
SazidKhan <khanmohdsazid@gmail.com>
2024-06-17 14:45:23 +0530

    New Build 6

--- server/index.js
+++ server/index.js
@@ -53,12 +53,45 @@ app.use("/api", authApiRouter);
 app.use("/api", apiRouter);
 
 // Handle all other routes and serve the index.html file
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const PORT = process.env.SERVER_PORT
+const MONGODB_URL = process.env.MONGO_URI
+const routes = require('./routes/routes');
+const path = require('path');
+const fs = require("fs")
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+mongoose.set("strictQuery", false);
+var bodyParser = require('body-parser')
+
+mongoose.connect(MONGODB_URL, () => {
+  console.log("MongoDB Connected Successfully!");
+});
+
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/api', routes)
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'remote') {
+  app.use(express.static(path.join(__dirname, '../client', 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', '/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
   })
 }
+
+app.listen(PORT, () => {
+  console.log(`API Server is running on ${PORT}`)
+}) 
 
 // If that above routes didnt work, we 404 them and forward to error handler
 //app.use(errorHandlers.notFound);
